202bbffd13349768192253bf18e9ddd71814fc60
italotec <italo.tecsaas@gmail.com>
2025-09-11 15:52:50 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Clone the GitHub repository
-git clone https://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@github.com/italotec/primos.git
+git clone "https://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@github.com/italotec/primos.git"
